92fc142a040e54e83171d2586a52daffc29570d7
J2D2Development <joedriscoll79@gmail.com>
2015-03-27 16:55:10 -0700

    More functions practice- trad for loops and higher order functions

--- functions2015-03-24.js
+++ functions2015-03-24.js
@@ -42,7 +42,7 @@ function strCombo(w) {
 //strCombo('dog');
 
 
-//Write a JavaScript function that returns a passed string with letters in alphabetical order. 
+//Write a JavaScript function that returns a passed string with letters in alphabetical order.
 function sortString(w) {
 	//split string into array to use the sort function
 	console.log(w.split('').sort().join(''));
@@ -96,4 +96,76 @@ function findLongest(str) {
 	}
 	console.log(finalLongest);
 }
-findLongest("Web Development Tutorial");
+//findLongest("Web Development Tutorial");
+
+
+
+// Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+// Note : As the letter 'y' can be regarded as both a vowel and a consonant, we do not count 'y' as vowel here.
+// Example string : 'The quick brown fox'
+// Expected Output : 5
+function vowelCount(str) {
+	var vowels = 'aeiou',
+		vowelArr = vowels.split(''),
+		strArr = str.split(''),
+		count = 0;
+
+	//traditional nested for loops
+	// for(var i = 0, l = str.length; i < l; i++) {
+	// 	for(var j = 0, len = vowels.length; j < len; j++) {
+	// 		if(str[i] === vowels[j]) {
+	// 			count += 1;
+	// 		}
+	// 	}
+	// }
+
+	//using higher order functions
+	strArr.forEach(function(c) {
+		vowelArr.forEach(function(x) {
+			if(c === x) { count += 1 }
+		});
+	});
+	console.log(count);
+}
+//vowelCount('The quick brown fox');
+
+
+
+// Write a JavaScript function that accepts a number as a parameter and check the number is prime or not.
+// Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+//type: Num->Boolean
+function primeCheck(n) {
+}
+
+
+//Write a JavaScript function which accepts an argument and returns the type.
+//Note : There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
+function typeCheck(arg) {
+	console.log(typeof arg);
+}
+// typeCheck(3);
+// typeCheck('string');
+// typeCheck(typeCheck);
+
+
+//Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string.
+//Sample arguments : 'w3resource.com', 'o'
+//Expected output : 2
+//type: (Str,Str->Num)
+function countLetters(string, letter) {
+	var count = 0,
+		strArray = string.split('');
+
+	//use for loop
+	// for(var i = 0, l = string.length; i < l; i++) {
+	// 	if(string[i] === letter) { count += 1 }
+	// }
+
+	//use higher order functions
+	strArray.forEach(function (x) {
+		if(x === letter) { count += 1 }
+	});
+
+	console.log(count);
+}
+countLetters('w3resource.com', 'o');
