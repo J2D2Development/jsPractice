6096396bdf01ce45d67fa7597304a21ae6246dea
J2D2Development <joedriscoll79@gmail.com>
2015-04-08 17:15:24 -0700

    Added more problems to do!

--- functions2015-03-24.js
+++ functions2015-03-24.js
@@ -530,3 +530,138 @@ tellTime();
 
 // Write a JavaScript program to calculate the area and perimeter of a circle.
 // Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
+
+// Write a JavaScript function to check whether an `input` is an array or not.
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// true
+
+// Write a JavaScript function to clone an array.
+// Test Data :
+// console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
+
+// Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+// Test Data :
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+// Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array.
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+// Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+// Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.
+
+// Write a JavaScript program to sort the items of an array.
+// Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+// Write a JavaScript program to find the most frequent item of an array.
+// Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Sample Output : a ( 5 times )
+
+// Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+// Write a JavaScript program which prints the elements of the following array.
+// Note : Use nested for loops.
+// Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// Sample Output :
+// "row 0"
+// " 1"
+// " 2"
+// " 1"
+// " 24"
+// "row 1"
+
+// Write a JavaScript program to find the sum of squares of a numeric vector.
+
+// Write a JavaScript program to compute the sum and product of an array of integers.
+
+// Write a JavaScript program to add items in an blank array and display the items.
+// Sample Screen :
+// add elements in an blank array
+
+// Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity).
+
+// We have the following arrays
+// color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+// o = ["th","st","nd","rd"]
+// Write a JavaScript program to display the colors in the following way :
+// "1st choice is Blue ."
+// "2nd choice is Green."
+// "3rd choice is Red."
+// - - - - - - - - - - - - -
+// Note : Use ordinal numbers to tell their position.
+
+// Find the leap years in a given range of years.
+
+// Write a JavaScript program to shuffle an array.
+
+// Write a JavaScript program to perform a binary search.
+// Note : A binary search or half-interval search algorithm finds the position of a specified input value within an array sorted by key value.
+// Sample array :
+// var items = [1, 2, 3, 4, 5, 7, 8, 9];
+// Expected Output :
+// console.log(binary_Search(items, 1)); //0
+// console.log(binary_Search(items, 5)); //4
+
+// There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
+// Sample array :
+// array1 = [1,0,2,3,4];
+// array2 = [3,5,6,7,8,13];
+// Expected Output :
+// [4, 5, 8, 10, 12, 13]
+
+// Write a JavaScript program to find duplicate values in a JavaScript array
+
+// Write a JavaScript program to flatten a nested (any depth) array. If you pass shallow, the array will only be flattened a single level.
+// Sample Data :
+// console.log(flatten([1, [2], [3, [[4]]],[5,6]]));
+// [1, 2, 3, 4, 5, 6]
+// console.log(flatten([1, [2], [3, [[4]]],[5,6]], true));
+// [1, 2, 3, [[4]], 5, 6]
+
+// Write a JavaScript program to compute the union of two arrays.
+// Sample Data :
+// console.log(union([1, 2, 3], [100, 2, 1, 10]));
+// [1, 2, 3, 10, 100]
+
+// Write a JavaScript function to find the difference of two arrays.
+// Sample Data :
+// console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+// console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+// ["1", "2", "3", "10", "100"]
+// ["1", "2", "3", "4", "5", "6"]
+
+// Write a JavaScript function that accepts an array and a delimiter and returns a string containing the array elements with the delimiter string between each element.
+// Test Data :
+// console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+// console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]]));
+// ["1", "2", "3", "10", "100"]
+// ["1", "2", "3", "4", "5", "6"]
